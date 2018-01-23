f68f775fdeb3360bbe15e9ac53a46bdbe07ff363
uraniumreza <reza.uranium@gmail.com>
2018-01-23 13:48:05 +0600

    scale homepage

---
+++ source/styles/Home.js
@@ -0,0 +1,35 @@
+/**
+ * React Native Boilerplate
+ * Developed By: Dubin Labs Ltd.
+ * Author: Nayeem Reza || Mostafiz Rahman
+ * https://github.com/mostafiz93/RNBoilerplate
+ */
+
+import { StyleSheet } from 'react-native';
+import { moderateScale } from '../helpers/Scale';
+import { primaryColor } from '../const/colors';
+
+export default StyleSheet.create({
+  container: {
+    position: 'relative',
+    flex: 1,
+    backgroundColor: '#F0F5F6',
+  },
+  content: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: moderateScale(20),
+    textAlign: 'center',
+    margin: moderateScale(10),
+  },
+  instructions: {
+    textAlign: 'center',
+    color: primaryColor,
+    fontSize: moderateScale(13),
+    marginBottom: moderateScale(5),
+  },
+});
